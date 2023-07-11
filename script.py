0f680512eff310576d1c58ebd7465c69e059fbea
Aidan Pugh <116562139+Aidan-Pugh@users.noreply.github.com>
2023-07-11 18:02:36 +0100

    Update script usability.py
    > Formatted functions
    > Added functions for file checking

--- script.py
+++ script.py
@@ -1,54 +1,84 @@
 import dendropy
 
-# Given a tree determine taxa list [.]
-# Prune tree to only contain said taxa [.]
-# Run AU tests [ ]
-
-# input_paths = [["constrained_data.fas", "gene_tree.txt", "concat_tree.txt", "constrained_tree.txt"],["constrained_data.fas", "gene_tree_2.txt", "concat_tree.txt", "constrained_tree.txt"],["constrained_data.fas", "gene_tree.txt", "gene_tree_2.txt", "concat_tree.txt", "constrained_tree.txt"]]
-input_paths = [["AA_dataset.fas", "AA_gene_tree.txt.contree", "AA_concat.txt.contree", "AA_constrained.txt.contree"]]
-test_names = ["AidanTrees"]
+#################
+### FUNCTIONS ###
+#################
 
 def concat_trees(trees):
+    """
+    Collect all the pruned trees together into a single object
+    This is then used to write the .treels file
+
+    > trees : List of trees in a newick format
+    """
     out = ""
     for tree in trees.values():
         out += tree.as_string(schema='newick') + "\n"
     return out
 
 
-# Read Newick format tree file
 def read_newick(filepath):
+    """
+    Puts the Newick tree read in from a .contree file into a computer friendly tree object
+
+    > filepath : the name of the raw unpruned file containing a newick format tree
+    """
     tree = dendropy.Tree.get(path=filepath, schema='newick', preserve_underscores=True)
-    print(f"Loaded tree: {filepath}")
-    # print(tree.as_string(schema='newick'))
+    print(f">>> Loaded tree: {filepath}")
+    # print("printing tree.as_string from read_newick file:",tree.as_string(schema='newick'))
     # print(tree.as_ascii_plot())
     return tree
 
+
 def find_common_taxa(taxa_sets):
-    # Get the set of items present in the first list
-    unique_items = set(taxa_sets[0])
+    """
+    Take the taxa_set and identify the common taxa across all sets.
+    common taxa then written to a list called unique_items_list
+    List is used to prune trees to common taxa
+
+    > taxa_sets : list of tree labels extracted from the newick tree
+    """
+    # An object containing sets of all the taxa found in each of the input newicks
+    unique_items = set(taxa_sets[0]) 
 
     # Iterate over the remaining lists
     for taxa_set in taxa_sets[1:]:
-        # Update the set of unique items by taking the intersection with each list
-        unique_items.intersection_update(taxa_set)
+        # .intersection_update updates unique_items with common taxa from all lists
+        unique_items.intersection_update(taxa_set) 
 
     # Convert the set back to a list
     unique_items_list = list(unique_items)
-
+    print(">>> Number of common taxa:",len(unique_items_list))
     return unique_items_list
 
+
 def get_labels_from_tree(tree):
+    """
+    Function which returns the labels of the newick tree as an object
+
+    > tree : A newick format tree 
+    """
     return[str(item.label) for item in tree.taxon_namespace]
 
-# Input tree files
-# treefiles = ["gene_tree.txt", "gene_tree_2.txt", "concat_tree.txt", "constrained_tree.txt"]
+
 def find_pruned_trees(treefiles):
+    """
+    Prune the trees inputted, and save them to new files.
+    Also prune the aligned dataset to a new file
+
+    > treefiles : 
+    """
     tree_labels = {}
     trees = {}
     pruned_trees = {}
 
-    alignment_file = treefiles[0]
-    treefiles.pop(0)
+    #alignment_file = treefiles[0] # Always put the aligned and trimmed data first
+    for f in treefiles:
+        if f.endswith(".fas"):
+            alignment_file = f
+            treefiles.remove(f)
+
+    #treefiles.pop(0) # remove it from the treefiles list
 
     for filepath in treefiles:
         tree = read_newick(filepath)
@@ -57,12 +87,10 @@ def find_pruned_trees(treefiles):
 
     # print(tree_labels.values())
     common_taxa = find_common_taxa(list(tree_labels.values()))
-    print("Found taxa in common:")
-    # print(common_taxa)
 
     for filepath in treefiles:
         
-        # find taxa to remove
+        # find taxa to remove based on the retrieved common taxa
         taxa_to_be_pruned = [item for item in tree_labels[filepath] if item not in common_taxa]
 
         # Make pruned version
@@ -72,14 +100,11 @@ def find_pruned_trees(treefiles):
         pruned_trees[filepath].prune_taxa_with_labels(taxa_to_be_pruned)
 
         # Save copy to file
-        pruned_trees[filepath].write(path=filepath + "_pruned.tre", schema='newick')
-        print("Pruned " + filepath)
-        # print(pruned_trees[filepath].as_ascii_plot())
+        pruned_trees[filepath].write(path= "0PRUNED_" + filepath + ".tre", schema='newick')
+        print(">>> Pruned " + filepath)
 
-
-    print(common_taxa)
-
-    print("Generating alignment file")
+    # Prune the alignment
+    print(">>> Generating alignment file")
     pruned_alignment = ""
     with open(alignment_file, "r") as f:  
         for line in f:
@@ -97,70 +122,44 @@ def find_pruned_trees(treefiles):
     with open(alignment_file + "_pruned.fas", "w") as f:
         f.write(pruned_alignment)
 
-    print("Generated alignment file")
+    print(">>> Pruned concat dataset alignment file")
 
     return pruned_trees
 
 
-# START SCRIPT
+def check_inputs(file_list):
+    """
+    Checks that the inputted files are valid
 
+    > file_list : 1 .fas file and all .contree/.tree files being pruned
+    """
+    count = 0
+    fas_files = []
+    for f in file_list:
+        for i in f:
+            print(i)
+            if i.endswith(".fas"):
+                count += 1
+                fas_files.append(i)
+    if count > 1:
+        raise ValueError(f"We can only have one '.fas' file, but you have submitted {count}: {fas_files}")
+
+##############
+### SCRIPT ###
+##############
+
+# List all the files being looked at
+input_paths = [["gene_data.fas", "z_coi_gene_tree.txt.contree", "concat_tree.txt.contree", "constrained_tree_2.txt.contree"]]
+check_inputs(input_paths)
+
+# This is the name of the .treels file which will contain all the pruned trees (extensions will be added automatically)
+test_names = ["0_all_trees"]
 
 for i in range(len(input_paths)):
     with open(test_names[i] + "_pruned.treels", 'w') as f:
 
-        print(">>> Finding Pruned Trees for " + str(", ".join(input_paths[i])))
+        print(">>> Finding Pruned Trees for: " + str(", ".join(input_paths[i])), "\n")
         pruned_trees = find_pruned_trees(input_paths[i])
         f.write(concat_trees(pruned_trees))
 
-print("Script complete")
-
-
-
-
-
-
-
-
-# # Load gene tree
-# gene_tree = read_newick("gene_tree.txt")
-# gene_tree_taxa = get_taxa_labels(gene_tree)
-# print("Gene Tree")
-# print(gene_tree.as_ascii_plot())
-# print("Gene tree labels")
-# print(gene_tree.as_string(schema='newick'))
-# print(gene_tree_taxa)
-
-# # Load concat tree
-# concat_tree = read_newick("concat_tree.txt")
-# concat_tree_taxa = get_taxa_labels(concat_tree)
-# print("Concat tree labels")
-# print(concat_tree.as_string(schema='newick'))
-# print(concat_tree_taxa)
-
-# # Create a concat copy 'Pruned'
-# print("Concat Tree (Prune copy)")
-# pruned_tree = dendropy.Tree(concat_tree)
-# pruned_taxa = pruned_tree.taxon_namespace
-# print(pruned_tree.as_ascii_plot())
-
-# # Determine labels to remove
-# # gene_tree_taxa_labels = [item.label for item in gene_tree_taxa]
-# # concat_tree_taxa_labels = [item.label for item in concat_tree_taxa]
-
-# # taxa_to_be_pruned = [item for item in pruned_taxa if item.label not in gene_tree_taxa_labels]
-# # print("Labels to remove...")
-# # prune_tree_taxa_labels = [str(item.label) for item in taxa_to_be_pruned]
-# # print(prune_tree_taxa_labels)
-
-# pruned_tree.prune_taxa(taxa_to_be_pruned) # Prune tree
-
-# # Print pruned tree to screen
-# print("Pruned Tree:")
-# print(get_taxa_labels(pruned_tree))
-# print(pruned_tree.as_string(schema='newick'))
-# print(pruned_tree.as_ascii_plot())
-
-# # Write to file
-# pruned_tree.write(path='pruned_tree.nwk', schema='newick')
-
-# print("Script Complete")
+print(">>>> Script complete <<<")
