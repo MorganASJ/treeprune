52aaa24aa75130f6460b9eee36047906938a890b
Aidan Pugh <116562139+Aidan-Pugh@users.noreply.github.com>
2023-07-13 16:56:27 +0100

    Update au_test.sh

--- au_test.sh
+++ au_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# change the model to whatever is suited
 iqtree2 -s *.pruned.fas -m GTR+F+G -z all_trees.pruned.treels -te *02_concat_tree.phy.contree.pruned.tre -zb 10000 -au
 
 exit 0
